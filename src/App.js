eb608fc1a3268b4454cfa2a4dfc4099fb6dfda99
piyuGH <piyushmpatel@live.com>
2021-04-05 11:19:01 +0530

    Signed-off-by: piyuGH <piyushmpatel@live.com>

--- src/App.js
+++ src/App.js
@@ -1,25 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
+import { CardList } from "./components/card-list/card-list.component";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      string: "Hello Piyush",
+      monsters: [
+        {
+          name: "Frankinstine",
+          id: "a001",
+        },
+        {
+          name: "Dracula",
+          id: "a002",
+        },
+        {
+          name: "Zombiee",
+          id: "a003",
+        },
+      ],
+    };
+  }
+  componentDidMount() {
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then((response) => response.json())
+      .then((users) => this.setState({ monsters: users }));
+    //.then((users) => console.log(users));
+  }
+
+  render() {    
+    return (
+      <div className="App">        
+        <header className="App-header">
+          <p>{this.state.string}</p>
+          <button onClick={() => this.setState({ string: "Hello Patel" })}>
+            Change Text
+          </button>
+        </header>
+        {this.state.monsters.map((monster) => (
+          <h1 key={monster.id}>{monster.name}</h1>
+        ))}
+      </div>
+    );
+  }
 }
 
 export default App;
